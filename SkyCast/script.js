197cdf49cda35b88213c6f228b8845c6e08af67c
Anmol Sangha <108411482+Sanghanmol@users.noreply.github.com>
2023-06-16 17:50:36 +0530

    Update script.js

--- SkyCast/script.js
+++ SkyCast/script.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY = ''; // your api-key
+const API_KEY = process.env.API_KEY; // your api-key
 
 setInterval(() => {
     const time = new Date();
